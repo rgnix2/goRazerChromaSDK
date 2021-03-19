72072f9a22bd606677f667ef93779d7b40f0f08d
Ralph G <rgnix2@gmail.com>
2021-03-19 01:50:18 -0400

    testing

--- razerChromaSDK.go
+++ razerChromaSDK.go
@@ -1,10 +1,53 @@
 package goRazerChromaSDK
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
+}
+
+type version struct {
+	Core    string `json:"core"`
+	Device  string `json:"device"`
+	Version string `json:"version"`
+}
+
+func getVersion() (version, error) {
+
+	url := "http://localhost:54235/razer/chromasdk"
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	curVersion := version{}
+	err = json.Unmarshal(responseData, &curVersion)
+	if err != nil {
+		return version{}, err
+	}
+	return curVersion, nil
+	// var verResp version
+	// json.Unmarshal(responseData, &verResp)
+	// fmt.Printf("%+v\n", verResp)
+
+	// return jsonData
+	//fmt.Println(string(responseData))
 }
