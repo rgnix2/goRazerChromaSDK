768fe6b90ea37b960617325babd56fcf854a7148
Ralph G <rgnix2@gmail.com>
2021-03-20 23:30:09 -0400

    fix: posting to api does not work add put

--- razerChromaSDK.go
+++ razerChromaSDK.go
@@ -4,13 +4,20 @@ package goRazerChromaSDK
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
+var sessionUrl = ""
+
 const (
-	api string = "http://localhost:54235/razer/chromasdk"
+	api                string = "https://chromasdk.io:54236/razer/chromasdk"
+	apiSession         string = "ads"
+	MaxIdleConnections int    = 20
+	RequestTimeout     int    = 5
 )
 
 type version struct {
@@ -35,6 +42,171 @@ type AppInfo struct {
 type sessionId struct {
 	Sessionid int    `json:"sessionid"`
 	URI       string `json:"uri"`
+}
+
+type Results struct {
+	ID     string `json:"id"`
+	Result int    `json:"result"`
+}
+
+type StaticColor struct {
+	Effect string `json:"effect"`
+	Param  struct {
+		Color int `json:"color"`
+	} `json:"param"`
+}
+
+func KeyboardStaticPUT(app AppInfo, color StaticColor) (Results, error) {
+	println("pgkURL: ", sessionUrl)
+	reqBody, err := json.Marshal(color)
+	println("reqbody:", string(reqBody))
+	if err != nil {
+		panic(err)
+	}
+	println("\napi: ", sessionUrl+"/keyboard")
+	///////////////////////////////////
+	// initialize http client
+	client := &http.Client{}
+
+	// set the HTTP method, url, and request body
+	req, err := http.NewRequest("PUT", sessionUrl+"/keyboard", bytes.NewBuffer([]byte(reqBody)))
+	if err != nil {
+		panic(err)
+	}
+
+	// set the request header Content-Type for json
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	//////////////
+	fmt.Println(resp.StatusCode, resp.Request.URL)
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+	fmt.Println("stringBody ", string(body))
+	curResults := Results{}
+	err = json.Unmarshal(body, &curResults)
+	if err != nil {
+		return Results{}, err
+	}
+
+	return curResults, nil
+}
+func MouseStatic(app AppInfo, color StaticColor) (Results, error) {
+	// func MouseStatic() {
+	// 	client := &http.Client{
+	// 		Transport: &http.Transport{
+	// 			MaxIdleConnsPerHost: MaxIdleConnections,
+	// 		},
+	// 		Timeout: time.Duration(RequestTimeout) * time.Second,
+	// 	}
+
+	// 	jsonString := fmt.Sprintf(`{"effect": "CHROMA_NONE"}`)
+	// 	print(jsonString)
+	// 	//newColor := StaticColor{Effect: "CHROMA_NONE"}
+	// 	//reqBody, err := json.Marshal(newColor)
+	// 	// /time.Sleep(2 * time.Second)
+	// 	req, err := http.NewRequest("PUT", sessionUrl+"/mousepad", bytes.NewBuffer([]byte(jsonString)))
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		//return err
+	// 	}
+
+	// 	req.Header.Set("Content-Type", "application/json")
+	// 	resp, err := client.Do(req)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		//	return err
+	// 	}
+	// 	fmt.Println(resp.StatusCode, resp.Request.URL)
+	// 	//	return nil
+	//newColor := StaticColor{Effect: "CHROMA_STATIC", StaticColor.Param{Color: 255}}
+	println("pgkURL: ", sessionUrl)
+	reqBody, err := json.Marshal(color)
+	println("reqbody:", string(reqBody))
+	if err != nil {
+		panic(err)
+	}
+	println("\napi: ", sessionUrl+"/mousepad")
+	///////////////////////////////////
+	// initialize http client
+	client := &http.Client{}
+
+	// set the HTTP method, url, and request body
+	req, err := http.NewRequest("PUT", sessionUrl+"/mousepad", bytes.NewBuffer([]byte(reqBody)))
+	if err != nil {
+		panic(err)
+	}
+
+	// set the request header Content-Type for json
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	//////////////
+	fmt.Println(resp.StatusCode, resp.Request.URL)
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+	fmt.Println("stringBody ", string(body))
+	curResults := Results{}
+	err = json.Unmarshal(body, &curResults)
+	if err != nil {
+		return Results{}, err
+	}
+
+	// appData, err := json.Marshal(color)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// print("appD: ", string(appData))
+
+	return curResults, nil
+}
+
+func KeyboardStatic(app AppInfo, color StaticColor) (Results, error) {
+	//newColor := StaticColor{Effect: "CHROMA_STATIC", StaticColor.Param{Color: 255}}
+	println("pgkURL: ", sessionUrl)
+	reqBody, err := json.Marshal(color)
+	println("reqbody:", string(reqBody))
+	if err != nil {
+		panic(err)
+	}
+	println("\napi: ", sessionUrl+"/keyboard")
+	resp, err := http.Post(sessionUrl+"/keyboard", "application/json", bytes.NewBuffer(reqBody))
+	//fmt.Println("postBody:", resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+	fmt.Println("stringBody ", string(body))
+	curResults := Results{}
+	err = json.Unmarshal(body, &curResults)
+	if err != nil {
+		return Results{}, err
+	}
+
+	// appData, err := json.Marshal(color)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// print("appD: ", string(appData))
+
+	return curResults, nil
 }
 
 func GetSession(app AppInfo) (sessionId, error) {
@@ -65,7 +237,11 @@ func GetSession(app AppInfo) (sessionId, error) {
 	if err != nil {
 		return sessionId{}, err
 	}
+	sessionUrl = curSession.URI
+
+	print("\npkgSession ", sessionUrl+"\n")
 	//fmt.Println("stringBody2 ", string(body))
+	time.Sleep(2 * time.Second)
 	return curSession, nil
 
 }
